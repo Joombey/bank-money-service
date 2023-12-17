fe15bf052de718287eb25212aa68e5e57a9a9e28
farukh <joombey@yandex.ru>
2023-12-17 18:38:23 +0300

    monitoring to grafana integrated

--- http/server.go
+++ http/server.go
@@ -4,10 +4,19 @@ import (
 	cts "farukh.go/money/constants"
 	hr "farukh.go/money/http/handlers"
 	"github.com/gin-gonic/gin"
+	"github.com/Depado/ginprom"
 )
 
 func Run() {
 	router := gin.Default()
+
+	p := ginprom.New(
+		ginprom.Engine(router),
+		ginprom.Subsystem("gin"),
+		ginprom.Path("/metrics"),
+	)
+	router.Use(p.Instrument())
+
 	router.POST(cts.TransferMoneyRoute, hr.TransferMoney)
 	router.POST(cts.LoadMoneyRoute, hr.LoadMoneyHandler)
 	router.GET(cts.GetValueRoute, hr.GetValueHandler)
